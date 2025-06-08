8fe4267a283e4b669ff89cdccbbde039b4e26f16
LucaCiani <lucaciani88@gmail.com>
2025-06-08 10:49:30 +0200

    complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,10 @@ const pricePerKm = 0.21;
 
 const price = pricePerKm * parseInt(distance.value);
 
+const carriage = Math.floor(Math.random() * 10) + 1;
+
+const code = Math.floor(10000 + Math.random() * 90000);
+
 let finalPrice;
 
 sendButton.addEventListener("click", (e) => {
@@ -19,11 +23,29 @@ sendButton.addEventListener("click", (e) => {
     const price = pricePerKm * parseInt(distance.value);
     if (age.value == 1) {
         finalPrice = price - price * 0.2;
+        document.getElementById("discount").innerHTML =
+            "Biglietto Minorenne (Sconto 20%)";
     } else if (age.value == 3) {
         finalPrice = price - price * 0.4;
+        document.getElementById("discount").innerHTML =
+            "Biglietto Anziano (Sconto 40%)";
     } else {
         finalPrice = price;
+        document.getElementById("discount").innerHTML = "Biglietto Standard";
     }
-    console.log(finalPrice.toFixed(2));
     document.getElementById("sendName").innerHTML += name.value;
+    document.getElementById("carriage").innerHTML += carriage;
+    document.getElementById("cpCode").innerHTML += code;
+    document.getElementById("price").innerHTML += finalPrice.toFixed(2);
+});
+
+resetButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    document.getElementById("name").value = "";
+    document.getElementById("distance").value = "";
+    document.getElementById("sendName").innerHTML = "";
+    document.getElementById("discount").innerHTML = "";
+    document.getElementById("carriage").innerHTML = "";
+    document.getElementById("cpCode").innerHTML = "";
+    document.getElementById("price").innerHTML = "";
 });
